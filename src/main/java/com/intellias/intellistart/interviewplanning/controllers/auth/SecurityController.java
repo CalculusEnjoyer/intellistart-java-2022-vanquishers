aaa054656e09033d547c2af95afa1165d2a124bf
grata_salve <80597907+grata-salve@users.noreply.github.com>
2022-10-19 00:58:33 +0300

    :heavy_plus_sign: added dependency for Spring OAuth2 connect. (#56)
    :sparkles: Connected OAuth2 to app and created controller to take information about authenticated user.
    :bug: added annotation "Repository" to 2 classes where it was missed.

---
+++ src/main/java/com/intellias/intellistart/interviewplanning/controllers/auth/SecurityController.java
@@ -0,0 +1,28 @@
+package com.intellias.intellistart.interviewplanning.controllers.auth;
+
+
+import java.security.Principal;
+import java.util.Map;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Candidate controller.
+ */
+@RestController
+@RequestMapping("/user")
+public class SecurityController {
+
+  /**
+   * Method to take info about authenticated user.
+   *
+   * @return information.
+   */
+  @GetMapping
+  public String getUser(final Principal user) {
+    Map map = (Map) ((OAuth2Authentication) user).getUserAuthentication().getDetails();
+    return "" + map;
+  }
+}
--- src/main/java/com/intellias/intellistart/interviewplanning/repositories/UserRepository.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/repositories/UserRepository.java
@@ -2,11 +2,13 @@ package com.intellias.intellistart.interviewplanning.repositories;
 
 import com.intellias.intellistart.interviewplanning.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * User repository.
  */
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
