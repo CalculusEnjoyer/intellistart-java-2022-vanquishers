184ba3c809641169ac3b9e0cc84f2d10481fc146
Olexandra <shokotko.olexandra@gmail.com>
2022-10-19 18:04:26 +0300

    create WeekServiceTest

---
+++ src/test/java/com/intellias/intellistart/interviewplanning/util/WeekServiceTest.java
@@ -0,0 +1,24 @@
+package com.intellias.intellistart.interviewplanning.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeekServiceTest {
+
+  @Autowired
+  WeekService weekService;
+
+  @Test
+  void getNextWeekNum() {
+    int nextWeek = weekService.getCurrentWeekNumInYear() + 1;
+    int currentYear = LocalDate.now().getYear();
+
+    assertThat(Integer.parseInt(String.valueOf(currentYear) + nextWeek))
+        .isEqualTo(weekService.getNextWeekNumInFormat());
+  }
+}
