b73292798659ce7e744c42fa4f54e7993d1f63b0
Vladyslav Oshovskyi <57325596+VladOSIO2@users.noreply.github.com>
2022-10-19 15:22:31 +0300

    Reintroduce PostgreSQL as main DB (#51) (#58)
    * Reintroduce PostgreSQL as main DB (#51)

--- src/test/java/com/intellias/intellistart/interviewplanning/services/InterviewerServiceTest.java
+++ src/test/java/com/intellias/intellistart/interviewplanning/services/InterviewerServiceTest.java
@@ -14,8 +14,10 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class InterviewerServiceTest {
   @Autowired
