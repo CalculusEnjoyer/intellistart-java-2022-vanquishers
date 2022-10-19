ef08eba529b1fd3955708fea913fe2bb7bae1a95
Vladyslav Oshovskyi <57325596+VladOSIO2@users.noreply.github.com>
2022-10-19 14:10:31 +0300

    :bricks: Implement template endpoints in CommonController & InterviewerController (#52)
    * :bricks: Implement template endpoints in CommonController & InterviewerController
    
    * Move endpoint 'update Interviewer time slot' to InterviewerController
    
    * Fix InterviewerController updateSlot endpoint & Fix PathVariable id var type

---
+++ src/main/java/com/intellias/intellistart/interviewplanning/controllers/CommonController.java
@@ -0,0 +1,47 @@
+package com.intellias.intellistart.interviewplanning.controllers;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Common controller.
+ * Manages the common, 'any user' endpoints for getting the basic information
+ * like current or next week, or info about current user
+ */
+
+@RestController
+public class CommonController {
+  /**
+   * Endpoint for getting the information about the current user.
+
+   * @return response status
+   */
+  @GetMapping("/me")
+  public ResponseEntity<HttpStatus> getMe() {
+    return ResponseEntity.ok(HttpStatus.OK);
+  }
+
+  /**
+   * Endpoint for getting the information about the current week number.
+
+   * @return response status
+   */
+  @GetMapping("/weeks/current")
+  public ResponseEntity<HttpStatus> getCurrentWeek() {
+    return ResponseEntity.ok(HttpStatus.OK);
+  }
+
+  /**
+   * Endpoint for getting the information about the next week number.
+
+   * @return response status
+   */
+  @GetMapping("/weeks/next")
+  public ResponseEntity<HttpStatus> getNextWeek() {
+    return ResponseEntity.ok(HttpStatus.OK);
+  }
+
+}
