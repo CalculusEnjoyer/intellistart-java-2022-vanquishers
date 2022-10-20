d4c2751a6f4e75966020299e11a5a2004827a7ef
Sergey Remez <s.remez-me23@lll.kpi.ua>
2022-10-20 16:10:32 +0300

    Change BookingService docs

--- src/main/java/com/intellias/intellistart/interviewplanning/services/BookingService.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/services/BookingService.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
- * Candidate entity service.
+ * Booking entity service.
  */
 @Service
 @Transactional
