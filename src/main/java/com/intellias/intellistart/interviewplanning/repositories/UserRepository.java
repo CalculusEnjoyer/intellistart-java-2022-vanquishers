9f5318fb7389ce77fdf4d5ed08f5e866cabf4716
grata_salve <80597907+grata-salve@users.noreply.github.com>
2022-10-25 22:48:31 +0300

    Connect OAuth2 to database. (#83)
    * :heavy_plus_sign: added dependency for Spring OAuth2 connect.
    :sparkles: Connected OAuth2 to app and created controller to take information about authenticated user.
    :bug: added annotation "Repository" to 2 classes where it was missed.
    
    * :arrow_up: Upgrade dependency.
    :wrench: Update application properties.
    :sparkles: User data is added to our database upon registration
    :white_check_mark: add email field in User model
    
    * :bricks: Implement template endpoints in CommonController & InterviewerController (#52)
    
    * :bricks: Implement template endpoints in CommonController & InterviewerController
    
    * Move endpoint 'update Interviewer time slot' to InterviewerController
    
    * Fix InterviewerController updateSlot endpoint & Fix PathVariable id var type
    
    * Reintroduce PostgreSQL as main DB (#51) (#58)
    
    * Reintroduce PostgreSQL as main DB (#51)
    
    * :arrow_up: Upgrade dependency.
    :wrench: Update application properties.
    :sparkles: User data is added to our database upon registration.
    :white_check_mark: add email field in User model.
    
    * :white_check_mark: now User can be registered and checked in database. We can take User's id and role.
    
    * :white_check_mark: now User can be registered and checked in database. We can take User's id and role.
    
    Co-authored-by: Vladyslav Oshovskyi <57325596+VladOSIO2@users.noreply.github.com>

--- src/main/java/com/intellias/intellistart/interviewplanning/repositories/UserRepository.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/repositories/UserRepository.java
@@ -10,5 +10,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+  public User findUserByFacebookId(Long facebookId);
 }
---
+++ src/main/java/com/intellias/intellistart/interviewplanning/services/UserService.java
@@ -0,0 +1,30 @@
+package com.intellias.intellistart.interviewplanning.services;
+
+import com.intellias.intellistart.interviewplanning.models.User;
+import com.intellias.intellistart.interviewplanning.repositories.UserRepository;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * User service.
+ */
+@Service
+@Transactional
+public class UserService {
+  private final UserRepository userRepository;
+
+  @Autowired
+  public UserService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  public void register(User user) {
+    userRepository.save(user);
+  }
+
+  public User findUserByFacebookId(Long facebookId) {
+    return userRepository.findUserByFacebookId(facebookId);
+  }
+
+}
