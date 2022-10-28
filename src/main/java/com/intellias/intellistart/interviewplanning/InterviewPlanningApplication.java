00327b3111e35b4420a700625095781a5c0bfd3c
CalculusEnjoyer <kravchukzxy@gmail.com>
2022-10-28 10:44:57 +0300

    Remove testing properties

--- src/main/java/com/intellias/intellistart/interviewplanning/InterviewPlanningApplication.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/InterviewPlanningApplication.java
@@ -3,14 +3,15 @@ package com.intellias.intellistart.interviewplanning;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 /**
  * Main application.
  */
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-//@EnableOAuth2Sso
+@SpringBootApplication
+@EnableOAuth2Sso
 public class InterviewPlanningApplication {
 
   public static void main(String[] args) {
