e10f5e0b8f1c4fd868601272e981811df58c6561
Olexandra <shokotko.olexandra@gmail.com>
2022-11-10 23:41:35 +0200

    Refactor createBooking() in CoordinatorController to not create booking that excess booking limit

--- src/main/java/com/intellias/intellistart/interviewplanning/repositories/BookingRepository.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/repositories/BookingRepository.java
@@ -1,7 +1,11 @@
 package com.intellias.intellistart.interviewplanning.repositories;
 
 import com.intellias.intellistart.interviewplanning.models.Booking;
+import com.intellias.intellistart.interviewplanning.models.Interviewer;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -11,4 +15,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
+  @Query(value = "select *\n"
+      + "from bookings b\n"
+      + "         join interviewer_slots i on i.id = b.interviewer_slots_id\n"
+      + "         join interviewers i2 on i2.id = i.interviewer_id\n"
+      + "where week_num = ?2 and i2.id = ?1",
+      nativeQuery = true)
+  List<Booking> findByInterviewerIdAndWeekNum(Long interviewerId, int weekNum);
 }
