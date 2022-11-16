2ef322df97d89a38b2bbcc6b8fdd91cdb47132e1
VladoSIO2 <vl4ds1mus@gmail.com>
2022-11-16 16:33:22 +0200

    :adhesive_bandage: Fix DB-related test failure
    - Fix test failure from tests accessing data created by other tests

--- src/test/java/com/intellias/intellistart/interviewplanning/controllers/InterviewerControllerTest.java
+++ src/test/java/com/intellias/intellistart/interviewplanning/controllers/InterviewerControllerTest.java
@@ -32,6 +32,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -43,6 +45,7 @@ import org.springframework.web.context.WebApplicationContext;
 @ExtendWith(SpringExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @AutoConfigureMockMvc(addFilters = false)
+@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
 class InterviewerControllerTest {
 
   @Autowired
