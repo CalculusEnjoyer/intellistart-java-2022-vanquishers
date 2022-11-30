113b98dfefb9f4c9c30fd31edd49ce66f8875c3a
Volodymyr Kravchuk <96874563+CalculusEnjoyer@users.noreply.github.com>
2022-11-30 16:03:13 +0200

    Add annotation for access of Coordinator (#151)
    Add annotation for access of Coordinator

--- src/test/java/com/intellias/intellistart/interviewplanning/controllers/CoordinatorControllerTest.java
+++ src/test/java/com/intellias/intellistart/interviewplanning/controllers/CoordinatorControllerTest.java
@@ -44,12 +44,17 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -62,6 +67,7 @@ import org.springframework.web.context.WebApplicationContext;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @AutoConfigureMockMvc(addFilters = false)
 @DirtiesContext(classMode = ClassMode.AFTER_CLASS)
+@WithMockUser(roles={"COORDINATOR"})
 class CoordinatorControllerTest {
 
   @Autowired
